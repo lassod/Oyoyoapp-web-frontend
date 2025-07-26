6f5376a25518b5be2d45f5da53cfc6ea9b46bacd
MAC <bibiweipere@gmail.com>
2025-07-26 16:57:29 +0100

    Updates

--- src/app/components/oyoyoLandingPage/Hero/Hero.tsx
+++ src/app/components/oyoyoLandingPage/Hero/Hero.tsx
@@ -6,27 +6,34 @@ import { Reveal1, Reveal3 } from "../../animations/Text";
 
 const HeroPage = () => {
   return (
-    <div className='bg-lines'>
-      <section className='rounded-[20px] h-auto max-w-[1380px] w-full max-h-[400px] top-[65px] mx-auto md:rounded-[50px]'>
-        <div className='z-10 flex flex-col justify-center items-center text-center relative top-[158px] gap-6 px-4 md:px-6'>
+    <div className="bg-lines">
+      <section className="rounded-[20px] h-auto max-w-[1380px] w-full max-h-[400px] top-[65px] mx-auto md:rounded-[50px]">
+        <div className="z-10 flex flex-col justify-center items-center text-center relative top-[158px] gap-6 px-4 md:px-6">
           <Reveal1>
-            <h1 className='max-w-[792px] text-center'>Transform Your Events with Oyoyo Events App</h1>
+            <h1 className="max-w-[792px] text-center">
+              Transform Your Events with Oyoyo Events App
+            </h1>
           </Reveal1>
-          <span className='text-red-700 font-bold'>Plan. Connect. Celebrate</span>
+          <span className="text-red-700 font-bold">
+            Plan. Connect. Celebrate
+          </span>
           <Reveal3>
-            <p className='max-w-[588px] w-full'>
-              Prepare to transform your planning journey with a platform crafted to simplify, streamline, and enhance
-              every facet of event management.
+            <p className="max-w-[588px] w-full">
+              Prepare to transform your planning journey with a platform crafted
+              to simplify, streamline, and enhance every facet of event
+              management.
             </p>
           </Reveal3>
-          <Link rel='noopener noreferrer' href='#contact'>
-            <Button variant={"landmark"} className='max-w-[142px]'>
-              Get the app
-            </Button>
+          <Link rel="noopener noreferrer" href="#contact">
+            <Button className="max-w-[142px]">Get the app</Button>
           </Link>
 
-          <div className='w-full md:mt-8 rounded-[10px] max-h-[300px] overflow-hidden'>
-            <Image src={bg} alt='BACKGROUND' className='bg-black shadow-lg rounded-xl' />
+          <div className="w-full md:mt-8 rounded-[10px] max-h-[300px] overflow-hidden">
+            <Image
+              src={bg}
+              alt="BACKGROUND"
+              className="bg-black shadow-lg rounded-xl"
+            />
           </div>
         </div>
       </section>
