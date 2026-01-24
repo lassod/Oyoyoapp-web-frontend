83c99236b91752df314b28de563ddb12202de86e
Bibiwei-Pere <bibiweipere@gmail.com>
2026-01-24 16:53:48 +0100

    DF

--- src/app/stream/[eventId]/page.tsx
+++ src/app/stream/[eventId]/page.tsx
@@ -1,17 +1,18 @@
-import LiveStream from "@/components/dashboard/events/LiveStream";
-
 export async function generateMetadata({ params }: any) {
   const event = await fetchEventData(params);
 
   if (!event)
     return {
       title: "Guest Event",
-      description: "Discover Oyoyo Events, the ultimate AI-powered event management platform...",
+      description:
+        "Discover Oyoyo Events, the ultimate AI-powered event management platform...",
     };
 
   return {
     title: event?.title || "Guest Event",
-    description: event?.description || "Discover Oyoyo Events, the ultimate AI-powered event management platform...",
+    description:
+      event?.description ||
+      "Discover Oyoyo Events, the ultimate AI-powered event management platform...",
     openGraph: {
       images: event?.media,
     },
@@ -19,11 +20,14 @@ export async function generateMetadata({ params }: any) {
 }
 
 async function fetchEventData(params: any) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_BACKEND_URL}/events/${params.eventId}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_BACKEND_URL}/events/${params.eventId}`,
+  );
   const json = await res.json();
   return json.data;
 }
 
 export default async function Page({ params }: any) {
-  return <LiveStream params={params} />;
+  // return <LiveStream params={params} />;
+  return <h2>Comming soon...</h2>;
 }
